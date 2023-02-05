bc7c1a9242c7d0f9ca66f1d688cf7b1404b0c1e8
manthanparekh <manthan.parekh@outlook.com>
2023-02-04 22:00:07 -0500

    proccing-data-from-a-json-file

---
+++ data_2_json.py
@@ -0,0 +1,22 @@
+import json
+from collections import namedtuple
+from functools import reduce
+
+city = namedtuple("city", ["name", "country", "coordinates", "continent"])
+
+with open('cities.json') as json_file:
+    data = json.load(json_file)
+
+cities = map(lambda x: city(x["name"], x["country"],
+                            x["coordinates"], x["continent"]), data["city"])
+
+asia = tuple(filter(lambda q: q.continent == "Asia", cities))
+
+print(asia)
+
+west = None
+
+west = reduce(
+    lambda x, y: x if x.coordinates[1] < y.coordinates[1] else y, asia)
+
+print(west)
